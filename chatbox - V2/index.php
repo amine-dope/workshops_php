657dfad9e0c682803098f5720167cbdea49892db
amine-dope <aminesailla85@gmail.com>
2020-06-29 11:09:45 +0100

    create index

---
+++ chatbox - V2/index.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'database.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+  		<meta charset="utf-8" />
+  		<title>CHATBOX</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+	</head>
+	<body>
+		<div id="container">
+			<header>
+				<h1>SHOUT IT!</h1>
+			</header>
+			<div id="shouts">
+			<ul>
+			  <?php
+			  	$sql = "SELECT id,user,message,time FROM shouts";
+				  $result = $conn->query($sql);
+				  while($row = $result->fetch_assoc()): ?>
+              <li class="shout"><span><?php echo $row['time']; ?> --- </span><strong><?php echo $row['user']; ?>:</strong> <?php echo $row['message']; ?> </li>
+             <?php endwhile; ?> </ul>
+				
+			</div>
+			<div id="input">
+				<form method="post" action="process.php">
+				<?php if(isset($_GET["error"])):?>
+                  <div class="error"><?php echo $_GET['error'];?> </div>
+				<?php endif;?>
+					<input type="text" name="user" placeholder="Enter Your Name" />
+					<input type="text" name="message" placeholder="Enter A Message" />
+					<br />
+					<input class="shout-btn" type="submit" name="submit" value="Throw it" />
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
