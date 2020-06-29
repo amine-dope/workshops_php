24b415da2d868524cec8253d39e4185396fa2b06
amine-dope <aminesailla85@gmail.com>
2020-06-29 11:09:24 +0100

    create database

---
+++ chatbox - V2/database.php
@@ -0,0 +1,15 @@
+<?php 
+$localhost="localhost";
+$username="root";
+$password="";
+$db="chatbox";
+
+$conn=new mysqli($localhost,$username,$password,$db);
+
+if($conn->connect_error){
+
+    printf("connecion failed : %s\n",$conn->connect_error.__LINE__);
+    exit();
+}
+
+?>
